87579b2c0947bce33b77526988e9b4e2f01149c0
omarankh <omarkhalili810@gmail.com>
2026-03-29 16:43:35 +0300

    feat: update API base URL configuration to support environment variables

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const apiBaseUrl = (import.meta.env.VITE_API_URL || "/api").replace(/\/$/, "");
+const api = axios.create({ baseURL: apiBaseUrl || "/api" });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
